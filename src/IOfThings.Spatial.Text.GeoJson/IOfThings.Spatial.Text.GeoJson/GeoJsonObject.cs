1a0e37e9548731b124ad21b304a19a5fb1d31d57
Guillaume Pelletier <guillaume.pelletier@dotvision.com>
2021-05-08 17:14:16 +0200

    Add some logic to bbox

--- src/IOfThings.Spatial.Text.GeoJson/IOfThings.Spatial.Text.GeoJson/GeoJsonObject.cs
+++ src/IOfThings.Spatial.Text.GeoJson/IOfThings.Spatial.Text.GeoJson/GeoJsonObject.cs
@@ -7,9 +7,16 @@ namespace IOfThings.Spatial.Text.GeoJson
         T[] _bbox;
         
         [JsonPropertyName(Json.PropertyNames.bbox)]
-        public T[] BBox { get => _bbox; set =>_bbox = value; }
+        public T[] BBox { get => ValidateBBox() ; set =>_bbox = value; }
         
         [JsonPropertyName(Json.PropertyNames.type)]
         public abstract GeoJsonType Type { get; }
+
+        internal T[] ValidateBBox()
+        {
+            return _bbox = _bbox??BuildBBox();
+        }
+
+        protected virtual T[] BuildBBox() => default;
     }
 }
--- src/IOfThings.Spatial.Text.GeoJson/IOfThings.Spatial.Text.GeoJson/GeoJsonPoint.cs
+++ src/IOfThings.Spatial.Text.GeoJson/IOfThings.Spatial.Text.GeoJson/GeoJsonPoint.cs
@@ -4,5 +4,13 @@ namespace IOfThings.Spatial.Text.GeoJson
     public class GeoJsonPoint<T> : GeoJsonSimpleGeometry<T>
     {
         public override GeoJsonType Type => GeoJsonType.Point;
+
+        protected override T[] BuildBBox() {
+
+            var box = new T[Coordinates.Length*2];
+            Coordinates.CopyTo(box, 0);
+            Coordinates.CopyTo(box, Coordinates.Length);
+            return box;
+        }
     }
 }
